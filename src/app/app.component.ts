27cb31a8cac4d6e30d07d53738f8dfbf5982d05d
Thao Nguyen <thao.nguyen@readytech.io>
2024-11-05 15:56:17 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'workshop_angular';
+  title = 'Workshop Angular';
+  baseUrl = 'https://localhost:7192/api/Home';
+  value = 1;
+
+  constructor(private http: HttpClient) {
+    
+  }
+
+  async add() {
+    await firstValueFrom(this.http.get<number>(this.baseUrl + '/add', {
+      params: {
+        value: this.value
+      }
+    })).then((data: number) => {
+      this.value = data;
+    });
+  }
+
+  async minus() {
+    await firstValueFrom(this.http.get<number>(this.baseUrl + '/minus', {
+      params: {
+        value: this.value
+      }
+    })).then((data: number) => {
+      this.value = data;
+    });
+  }
 }
